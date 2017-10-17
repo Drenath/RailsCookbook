be3d5398ad0559aca275115389700cd610266df8
Drenath <mvandyke@student.cvtc.edu>
2017-10-17 12:37:50 -0500

    Added project to github

---
+++ cookbook/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  resources :recipes
+  root to: 'home#index'
+  get '/about', to: 'home#about', as: 'about'
+  get '/contact', to: 'home#contact', as: 'contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
